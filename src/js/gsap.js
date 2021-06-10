d95b75aaf81ce05b8cd4066c5df636381e6d9484
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-06-10 20:04:20 +0200

    tweaked some animations

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -6,13 +6,12 @@ const tl = gsap.timeline();
 function initPortfolio() {
   tl.from('.introduction', {
     rotate: 50,
-    x: 600,
-    y: 100,
-    duration: 4,
-    stagger: 1,
+    x: 700,
+    y: 50,
+    duration: 3,
     scale: 0,
     opacity: 0,
-    ease: 'circ',
+    ease: 'back',
     onComplete: introductionLeave,
   });
 }
@@ -20,13 +19,53 @@ function initPortfolio() {
 function introductionLeave() {
   tl.to('.introduction', {
     rotate: -100,
-    x: 600,
+    x: 700,
     y: 50,
-    duration: 4,
-    stagger: 1,
+    stagger: 0,
+    duration: 2,
+    stagger: 0,
     scale: 0,
     opacity: 0,
+    onComplete: showHeadingPrimary,
+  });
+}
+
+function showHeadingPrimary() {
+  document.querySelector('.welcome-section__heading-primary').style.visibility =
+    'visible';
+  tl.from('.welcome-section__heading-primary', {
+    x: 0,
+    y: 0,
+    stagger: 0,
+    duration: 2,
+    scale: 2,
     ease: 'back',
+    opacity: 0,
+    onComplete: showRemainingElements,
+  });
+}
+
+//Show the remaining element in welcome section (index.html)
+function showRemainingElements() {
+  document.querySelector('.animated').style.visibility = 'visible';
+  tl.from('.animated', {
+    x: 0,
+    y: 0,
+    ease: 'back',
+    opacity: 0,
+    onComplete: initIcons,
+  });
+}
+
+function initIcons() {
+  tl.to('.icons__icon', {
+    x: 'random(-20, 20)', //chooses a random number between -20 and 20 for each target, rounding to the closest 5!
+    y: 'random(-20, 10)',
+    duration: 4,
+    ease: 'back',
+    repeat: -1,
+    opacity: 1,
+    repeatRefresh: true, // gets a new random x and y value on each repeat
   });
 }
 

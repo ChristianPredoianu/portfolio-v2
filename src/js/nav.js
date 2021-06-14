1e35fc5a787e8c4d13c379b41dbacfd08d48083f
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-06-14 12:44:58 +0200

    changed back col on hamburger lines when nav is open

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,4 +3,16 @@ const navLinks = document.getElementById('nav-links');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
+  changeHamburgerColor();
 });
+
+function changeHamburgerColor() {
+  const hamburgerLines = document.querySelectorAll('.hamburger__line');
+  hamburgerLines.forEach((hamburgerLine) => {
+    if (navLinks.classList.contains('open')) {
+      hamburgerLine.style.background = 'white';
+    } else {
+      hamburgerLine.style.background = '#0b16a8';
+    }
+  });
+}

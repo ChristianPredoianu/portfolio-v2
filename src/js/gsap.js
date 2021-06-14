7901c6f3b89dede2dad023544211dc14b75b9812
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-06-14 13:09:09 +0200

    added session storage for animation

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -2,33 +2,44 @@ import { gsap } from 'gsap';
 
 const tl = gsap.timeline({ defaults: { duration: 1 } });
 
-tl.from('.introduction', {
-  x: -1500,
-  y: 0,
-  scale: 0,
-  opacity: 0,
-  /* ease: 'back', */
-})
-  .to('.introduction', {
-    x: 900,
-    y: 50,
+const hasAnimationPlayed = sessionStorage.getItem('hasAnimationPlayed');
+
+if (!hasAnimationPlayed) {
+  tl.from('.introduction', {
+    x: -1500,
+    y: 0,
     scale: 0,
     opacity: 0,
+    duration: 1.5,
+    /* ease: 'back', */
   })
-  .from('.animated', {
-    y: -50,
-    stagger: 0.4,
-    opacity: 0,
-  })
-  .to('.icons__icon', {
-    x: 'random(-20, 20)', //chooses a random number between -20 and 20 for each target, rounding to the closest 5!
-    y: 'random(-20, 10)',
-    duration: 4,
-    ease: 'ease',
-    repeat: -1,
-    opacity: 1,
-    repeatRefresh: true, // gets a new random x and y value on each repeat
-  });
+    .to('.introduction', {
+      x: 900,
+      y: 50,
+      scale: 0,
+      opacity: 0,
+    })
+    .from('.animated', {
+      y: -50,
+      stagger: 0.4,
+      opacity: 0,
+      onComplete: () => {
+        sessionStorage.setItem('hasAnimationPlayed', true);
+      },
+    });
+} else {
+  document.querySelector('.introduction').style.display = 'none';
+}
+
+tl.to('.icons__icon', {
+  x: 'random(-20, 20)', //chooses a random number between -20 and 20 for each target, rounding to the closest 5!
+  y: 'random(-20, 10)',
+  duration: 4,
+  ease: 'ease',
+  repeat: -1,
+  opacity: 1,
+  repeatRefresh: true, // gets a new random x and y value on each repeat
+});
 
 //Hover effect on CTA (Projects-btn)
 const cta = document.querySelector('.welcome-section__cta');

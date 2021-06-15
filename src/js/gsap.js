6c96efe15a2ec2a60e05170c1cd41bce35a66906
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-06-15 15:25:00 +0200

    tweaked animations

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -4,32 +4,13 @@ const tl = gsap.timeline({ defaults: { duration: 1 } });
 
 const hasAnimationPlayed = sessionStorage.getItem('hasAnimationPlayed');
 
-if (!hasAnimationPlayed) {
-  tl.from('.introduction', {
-    x: -1500,
-    y: 0,
-    scale: 0,
-    opacity: 0,
-    duration: 1.5,
-    /* ease: 'back', */
-  })
-    .to('.introduction', {
-      x: 900,
-      y: 50,
-      scale: 0,
-      opacity: 0,
-    })
-    .from('.animated', {
-      y: -50,
-      stagger: 0.4,
-      opacity: 0,
-      onComplete: () => {
-        sessionStorage.setItem('hasAnimationPlayed', true);
-      },
-    });
-} else {
-  document.querySelector('.introduction').style.display = 'none';
-}
+tl.from('.animated', {
+  y: -50,
+
+  stagger: 0.6,
+  opacity: 0,
+  onComplete: animationEnded,
+});
 
 tl.to('.icons__icon', {
   x: 'random(-20, 20)', //chooses a random number between -20 and 20 for each target, rounding to the closest 5!
@@ -42,15 +23,20 @@ tl.to('.icons__icon', {
 });
 
 //Hover effect on CTA (Projects-btn)
-const cta = document.querySelector('.welcome-section__cta');
+function hoverOnCta() {
+  const cta = document.querySelector('.welcome-section__cta');
+  const hover = gsap.to('.welcome-section__cta', {
+    y: -3,
+    scale: 1.01,
+    duration: 0.2,
+    paused: true,
+  });
 
-const hover = gsap.to('.welcome-section__cta', {
-  y: 2,
-  scale: 1.01,
-  duration: 0.1,
-  paused: true,
-  ease: 'power1.inOut',
-});
+  cta.addEventListener('mouseenter', () => hover.play());
+  cta.addEventListener('mouseleave', () => hover.reverse());
+}
 
-cta.addEventListener('mouseenter', () => hover.play());
-cta.addEventListener('mouseleave', () => hover.reverse());
+function animationEnded() {
+  hoverOnCta();
+  sessionStorage.setItem('hasAnimationPlayed', true);
+}

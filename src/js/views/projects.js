41bacdb74ab0bb9ff294e384184b304da836e648
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-06-16 13:41:38 +0200

    changed folder for views js files

---
+++ src/js/views/projects.js
@@ -0,0 +1,6 @@
+import nav from '../nav';
+import typed from '../typed';
+
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';

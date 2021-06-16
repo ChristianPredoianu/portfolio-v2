ab73fab43bca0a5eda40fd1f8f7376deafe8034c
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-06-16 13:38:30 +0200

    changed folder

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-import nav from './nav';
-import gsap from './gsap';
-
-import '@fortawesome/fontawesome-free/css/brands.css';
-import '@fortawesome/fontawesome-free/css/fontawesome.css';
--- src/js/projects.js
+++
@@ -1,7 +0,0 @@
-import nav from './nav';
-import swiper from './swiper';
-import typed from './typed';
-
-import '@fortawesome/fontawesome-free/css/brands.css';
-import '@fortawesome/fontawesome-free/css/solid.css';
-import '@fortawesome/fontawesome-free/css/fontawesome.css';

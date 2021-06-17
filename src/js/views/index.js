6b4af3fffb4ae83b42eb58966f5eed6f5ee152ee
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-06-17 14:08:50 +0200

    imported solid icons - fontawesome

--- src/js/views/index.js
+++ src/js/views/index.js
@@ -2,4 +2,5 @@ import nav from '../nav';
 import gsap from '../gsap';
 
 import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
 import '@fortawesome/fontawesome-free/css/fontawesome.css';

ccd0eef595ecea1adf4ae14dc571ae2c5c706fc6
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-06-22 10:59:08 +0200

    added gsap animation and css rule plugin for pseudo el

--- src/js/views/about.js
+++ src/js/views/about.js
@@ -1,1 +1,36 @@
 import nav from '../nav';
+
+import { gsap } from 'gsap';
+import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
+gsap.registerPlugin(CSSRulePlugin);
+
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+
+const imgBefore = CSSRulePlugin.getRule('.img-container::before');
+const imgAfter = CSSRulePlugin.getRule('.img-container::after');
+
+gsap.to(imgBefore, {
+  duration: 3,
+
+  cssRule: {
+    y: -10,
+    x: 10,
+
+    opacity: 0.9,
+  },
+  yoyo: true,
+  repeat: -1,
+});
+
+gsap.to(imgAfter, {
+  duration: 3,
+  cssRule: {
+    x: -10,
+    y: 10,
+    opacity: 0.4,
+  },
+  yoyo: true,
+  repeat: -1,
+});

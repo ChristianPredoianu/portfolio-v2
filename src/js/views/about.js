a62f5a62c67e69a5d66f9374749d1ed6433a5a72
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-06-29 13:18:12 +0200

    made typed heading run only once with sessionstorage

--- src/js/views/about.js
+++ src/js/views/about.js
@@ -1,5 +1,6 @@
 import nav from '../nav';
-
+import Typed from 'typed.js';
+import { aboutHeadingOptions } from '../typedOptions';
 import { gsap } from 'gsap';
 import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
 gsap.registerPlugin(CSSRulePlugin);
@@ -7,6 +8,14 @@ gsap.registerPlugin(CSSRulePlugin);
 import '@fortawesome/fontawesome-free/css/brands.css';
 import '@fortawesome/fontawesome-free/css/solid.css';
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
+
+const isTypedAbout = sessionStorage.getItem('isTypedAbout');
+
+if (!isTypedAbout) {
+  const aboutHeadingTyped = new Typed('.about-typed', aboutHeadingOptions);
+} else {
+  document.querySelector('.about-typed').innerText = 'About Me';
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener(
@@ -17,11 +26,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
       gsap.to(imgBefore, {
         duration: 3,
-
         cssRule: {
           y: -10,
           x: 10,
-
           opacity: 0.9,
         },
         yoyo: true,

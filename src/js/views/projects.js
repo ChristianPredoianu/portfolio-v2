7a09f765bde93d1666a622ed47de0c31e289d590
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-06-29 13:21:40 +0200

    refactored typed code

--- src/js/views/projects.js
+++ src/js/views/projects.js
@@ -1,6 +1,33 @@
 import nav from '../nav';
-import typed from '../typed';
+import Typed from 'typed.js';
+
+import {
+  headingProjectsOptions,
+  swiperHeadingOneOptions,
+  swiperHeadingTwoOptions,
+} from '../typedOptions';
 
 import '@fortawesome/fontawesome-free/css/brands.css';
 import '@fortawesome/fontawesome-free/css/solid.css';
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
+
+const isTypedProjects = sessionStorage.getItem('isTypedProjects');
+
+// If headings are not typed type them out
+if (!isTypedProjects) {
+  const headingProjectsTyped = new Typed('.typed', headingProjectsOptions);
+  const swiperHeadingOneTyped = new Typed(
+    '.typed-swiper-heading-one',
+    swiperHeadingOneOptions
+  );
+  const swiperHeadingTwoTyped = new Typed(
+    '.typed-swiper-heading-two',
+    swiperHeadingTwoOptions
+  );
+} else {
+  document.querySelector('.typed').innerText = 'Projects';
+  document.querySelector('.typed-swiper-heading-one').innerText =
+    'JavaScript Projects';
+  document.querySelector('.typed-swiper-heading-two').innerText =
+    'Html/Css Projects';
+}

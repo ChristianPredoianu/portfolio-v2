9ba519bce55fc02ec32688a48512b63ba61bbd07
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-07-09 14:02:55 +0200

    added dark mode toggler

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,16 +1,17 @@
+import darkmode from './darkmode';
+
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
-  changeHamburgerColor();
+  /*  changeHamburgerColor(); */
 });
 
-function changeHamburgerColor() {
+/* function changeHamburgerColor() {
   const hamburgerLines = document.querySelectorAll('.hamburger__line');
   hamburgerLines.forEach((hamburgerLine) => {
-    navLinks.classList.contains('open')
-      ? (hamburgerLine.style.background = 'white')
-      : (hamburgerLine.style.background = '#0b16a8');
+    if (navLinks.classList.contains('open'))
+      hamburgerLine.style.background = 'white';
   });
-}
+} */

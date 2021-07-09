3376100b0b4f2cf7946cce53afbe09144448b7d4
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-07-09 14:02:25 +0200

    added dark mode

---
+++ src/js/darkmode.js
@@ -0,0 +1,30 @@
+const darkmodeToggler = document.querySelector('.theme-switch__input');
+const body = document.querySelector('body');
+let darkmode = localStorage.getItem('darkmode');
+
+window.addEventListener('load', () => {
+  darkmode = localStorage.getItem('darkmode');
+  if (darkmode === 'true') {
+    darkmodeToggler.checked = true;
+    enableDarkMode();
+  } else {
+    darkmodeToggler.checked = false;
+  }
+});
+
+function enableDarkMode() {
+  body.classList.add('darkmode');
+}
+
+function disableDarkMode() {
+  body.classList.remove('darkmode');
+}
+
+darkmodeToggler.addEventListener('click', () => {
+  darkmodeToggler.checked
+    ? localStorage.setItem('darkmode', true)
+    : localStorage.setItem('darkmode', false);
+
+  darkmode = localStorage.getItem('darkmode');
+  darkmode === 'true' ? enableDarkMode() : disableDarkMode();
+});

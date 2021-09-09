9e3affc0ef1ac3a3487dd2802ffc39c8e4d3efbb
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-09-09 10:16:28 +0200

    added js file for fontawesome icons to export

---
+++ src/js/icons.js
@@ -0,0 +1,24 @@
+export {
+  faGithub,
+  faFacebook,
+  faLinkedinIn,
+  faHtml5,
+  faCss3Alt,
+  faJsSquare,
+  faVuejs,
+  faSass,
+  faFigma,
+  faBootstrap,
+  faNpm,
+} from '@fortawesome/free-brands-svg-icons';
+
+export {
+  faTerminal,
+  faTools,
+  faMobile,
+  faCode,
+  faWind,
+  faBoxOpen,
+  faBox,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
